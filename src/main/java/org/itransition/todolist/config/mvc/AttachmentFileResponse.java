881466a0681d71f78eb2a333eadb90847d0a49e6
ashyshyma <alekseyshyshyma@gmail.com>
2022-01-06 11:31:31 +0200

    Created custom controller response java file(for preventing injecting HttpServletResponse inside controllers methods arguments and writing the duplicate code for adding content-disposition header)

---
+++ src/main/java/org/itransition/todolist/config/mvc/AttachmentFileResponse.java
@@ -0,0 +1,22 @@
+package org.itransition.todolist.config.mvc;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+
+/**
+ * Represents an HTTP response file,
+ * returns file to the client side
+ *
+ * @see AttachmentFileResponseMethodReturnHandler
+ */
+@Builder
+@Getter
+@Setter
+public class AttachmentFileResponse {
+
+    private byte[] content;
+    private String name;
+    private HttpStatus httpStatus;
+}
---
+++ src/main/java/org/itransition/todolist/config/mvc/WebMvcConfig.java
@@ -0,0 +1,16 @@
+package org.itransition.todolist.config.mvc;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodReturnValueHandler;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addReturnValueHandlers(List<HandlerMethodReturnValueHandler> handlers) {
+        handlers.add(new AttachmentFileResponseMethodReturnHandler());
+    }
+}
