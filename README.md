8066d078fa4e329ee69e52e12bab0d9aefe7252f
ashyshyma <alekseyshyshyma@gmail.com>
2022-02-02 14:53:59 +0200

    Fixed toggle thread for pull request from topic "Configured mysql test container"

--- README.md
+++ README.md
@@ -1,8 +1,4 @@
 ## "Task manager" application
 
 ### For execution need:
-- create such environment variables: DATASOURCE_URL, DATASOURCE_USERNAME, DATASOURCE_PASSWORD,
- TEST_DATASOURCE_URL, TEST_DATASOURCE_USERNAME, TEST_DATASOURCE_PASSWORD.
-
-- mysql timezone must be in 'UTC' format or the problem will
- occur during testing and application runtime.
+- create such environment variables: DATASOURCE_URL, DATASOURCE_USERNAME, DATASOURCE_PASSWORD
--- src/test/java/org/itransition/taskmanager/controller/rest/BaseControllerTest.java
+++ src/test/java/org/itransition/taskmanager/controller/rest/BaseControllerTest.java
@@ -6,7 +6,7 @@ import org.itransition.taskmanager.exception.ModelNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
@@ -15,9 +15,9 @@ import static java.util.Objects.requireNonNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @Getter
+@ActiveProfiles("test")
 @SpringBootTest
 @AutoConfigureMockMvc
-@TestPropertySource("/application.properties")
 public abstract class BaseControllerTest {
 
     private final String pageSizeParam = "size";
