25b4c3115627e70b48a2c052bc5057ad3ad56ed6
ashyshyma <alekseyshyshyma@gmail.com>
2022-02-07 00:49:44 +0200

    Created cron mail send service and constant classes

---
+++ src/main/java/org/itransition/taskmanager/constant/CronExpression.java
@@ -0,0 +1,9 @@
+package org.itransition.taskmanager.constant;
+
+public class CronExpression {
+
+    public static final String EVERY_DAY = "0 0 10 * * *";
+    public static final String EVERY_WEEK = "0 0 10 * * MON";
+    public static final String EVERY_MONTH = "0 0 10 1 * *";
+    public static final String EVERY_YEAR = "0 0 10 1 JAN *";
+}
