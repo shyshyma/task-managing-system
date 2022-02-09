394387cc8bd251f7b43dc88a1e25846da4b4e7f9
ashyshyma <alekseyshyshyma@gmail.com>
2022-02-09 14:14:03 +0200

    Configured spring async mechanism

--- src/main/java/org/itransition/taskmanager/TaskManagerApplication.java
+++ src/main/java/org/itransition/taskmanager/TaskManagerApplication.java
@@ -2,7 +2,9 @@ package org.itransition.taskmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @SpringBootApplication
 public class TaskManagerApplication {
     public static void main(String[] args) {
