7521440209d14094f85dff406ab2124d7f63d550
ashyshyma <alekseyshyshyma@gmail.com>
2022-02-09 14:29:14 +0200

    Removed crons and added them to enum 'NotificationFrequency'

--- src/main/java/org/itransition/taskmanager/jpa/entity/NotificationFrequency.java
+++ src/main/java/org/itransition/taskmanager/jpa/entity/NotificationFrequency.java
@@ -1,8 +1,16 @@
 package org.itransition.taskmanager.jpa.entity;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum NotificationFrequency {
-    EVERY_DAY,
-    EVERY_WEEK,
-    EVERY_MONTH,
-    EVERY_YEAR
+
+    EVERY_DAY("0 0 10 * * *"),
+    EVERY_WEEK("0 0 10 * * MON"),
+    EVERY_MONTH("0 0 10 1 * *"),
+    EVERY_YEAR("0 0 10 1 JAN *");
+
+    private final String cronExpression;
 }
