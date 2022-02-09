db5540308fbd3e9744fddfdbdafe9b79dd9f3872
ashyshyma <alekseyshyshyma@gmail.com>
2022-02-09 14:19:31 +0200

    Created new method which maps without id 'ConsumerJpaMapper.java'

--- src/main/java/org/itransition/taskmanager/mapper/ConsumerJpaMapper.java
+++ src/main/java/org/itransition/taskmanager/mapper/ConsumerJpaMapper.java
@@ -4,9 +4,13 @@ import org.itransition.taskmanager.dto.ConsumerDto;
 import org.itransition.taskmanager.jpa.entity.Consumer;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public abstract class ConsumerJpaMapper {
 
     public abstract Consumer map(ConsumerDto consumerDto);
+
+    @Mapping(target = "id", ignore = true)
+    public abstract void mapWithoutId(ConsumerDto consumerDto, @MappingTarget Consumer consumer);
 }
