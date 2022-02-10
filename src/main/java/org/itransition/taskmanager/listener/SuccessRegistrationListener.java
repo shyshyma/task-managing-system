de4c0ebda9f87cc0ed23a930f6dad81f3a1b26a2
ashyshyma <alekseyshyshyma@gmail.com>
2022-02-10 14:53:18 +0200

    Removed async from listener

--- src/main/java/org/itransition/taskmanager/listener/SuccessRegistrationListener.java
+++ src/main/java/org/itransition/taskmanager/listener/SuccessRegistrationListener.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.itransition.taskmanager.event.SuccessRegistrationEvent;
 import org.itransition.taskmanager.service.email.EmailService;
 import org.springframework.context.event.EventListener;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -13,7 +12,6 @@ public class SuccessRegistrationListener {
 
     private final EmailService emailService;
 
-    @Async
     @EventListener
     public void handleSuccessRegistrationEvent(SuccessRegistrationEvent event) {
         emailService.sendTemplateMessage(event.getEmailDetails());
