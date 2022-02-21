675c13a4d2f9c805a4d687f5d1509e418491404d
ashyshyma <alekseyshyshyma@gmail.com>
2022-02-21 16:24:15 +0200

    Made sending 'task log' messages inside of 'task' service

--- task-manager/src/main/java/org/itransition/taskmanager/jpa/dao/TaskRepository.java
+++ task-manager/src/main/java/org/itransition/taskmanager/jpa/dao/TaskRepository.java
@@ -3,6 +3,7 @@ package org.itransition.taskmanager.jpa.dao;
 import org.itransition.taskmanager.jpa.entity.Task;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -19,4 +20,7 @@ public interface TaskRepository extends BaseRepository<Task> {
     Page<Task> findByConsumerId(Long consumerId, Pageable pageable);
 
     void deleteByIdAndConsumerId(Long id, Long consumerId);
+
+    @Query("SELECT t.title from Task t WHERE t.id = ?1")
+    String findTaskTitleById(Long id);
 }
