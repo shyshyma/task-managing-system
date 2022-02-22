e8b0ab886c85bc226c3810a4192380f19ecf85ce
ashyshyma <alekseyshyshyma@gmail.com>
2022-02-22 11:35:18 +0200

    Fixed toggle threads

--- task-manager/src/main/java/org/itransition/taskmanager/constant/MessageBroker.java
+++ task-manager/src/main/java/org/itransition/taskmanager/constant/MessageBroker.java
@@ -1,39 +1,22 @@
 package org.itransition.taskmanager.constant;
 
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
 public final class MessageBroker {
 
-    private static final String NOT_BE_INITIALIZED_EXCEPTION_MESSAGE = "This class cannot be initialized";
-
-    private MessageBroker() {
-        throw new RuntimeException(NOT_BE_INITIALIZED_EXCEPTION_MESSAGE);
-    }
-
+    @UtilityClass
     public final class Queue {
-
-        private Queue() {
-            throw new RuntimeException(NOT_BE_INITIALIZED_EXCEPTION_MESSAGE);
-        }
-
         public static final String TASK_LOG_QUEUE_NAME = "task-log-q";
     }
 
-
+    @UtilityClass
     public final class Exchange {
-
-        private Exchange() {
-            throw new RuntimeException(NOT_BE_INITIALIZED_EXCEPTION_MESSAGE);
-        }
-
         public static final String TASK_LOG_EXCHANGE_NAME = "task-log-e";
     }
 
-
+    @UtilityClass
     public final class RoutingKey {
-
-        private RoutingKey() {
-            throw new RuntimeException(NOT_BE_INITIALIZED_EXCEPTION_MESSAGE);
-        }
-
         public static final String TASK_LOG_ROUTING_KEY_NAME = "task-log-rk";
     }
 }
